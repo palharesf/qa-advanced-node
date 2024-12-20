2726925fa29846bf6ec75c19c5b795fa0425163a
Fernando <palhares.f@gmail.com>
2024-12-20 14:08:00 -0300

    Database connection established

--- server.js
+++ server.js
@@ -11,13 +11,6 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {
-  res.render("index", {
-    title: "Hello",
-    message: "Please log in",
-  }); //Render the index.pug template
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
@@ -44,12 +37,28 @@ app.use(
 passport.initialize();
 passport.session();
 
+myDB(async client => {
+  const myDataBase = await client.db("database").collection("users");
+
+  app.route("/").get((req, res) => {
+    res.render("index", {
+      title: "Connected to Database",
+      message: "Please login",
+    });
+  });
+
 passport.serializeUser((user, done) => {
   done(null, user._id);
 });
 
 passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  // });
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  done(null, doc);
+  });
+});
+  
+  }).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
 });
