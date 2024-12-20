a62313c1b64256d872bd359c095d1d3723982bf9
Fernando <palhares.f@gmail.com>
2024-12-20 15:30:12 -0300

    Rendering profile page

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ myDB(async (client) => {
   );
 
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render("profile");
+    res.render("profile", { username: req.user.username });
   });
 
   passport.serializeUser((user, done) => {
