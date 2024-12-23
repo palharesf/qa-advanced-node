2cbc226ea052e164fce76ac303a3865c2305f5b6
Fernando <palhares.f@gmail.com>
2024-12-23 11:26:48 -0300

    Fixed implementation of auth and routes.js within export module brackets

--- auth.js
+++ auth.js
@@ -3,32 +3,32 @@ const LocalStrategy = require("passport-local");
 const bcrypt = require("bcrypt");
 const ObjectID = require("mongodb").ObjectID;
 
-module.exports = function (app, myDataBase) { };
+module.exports = function (app, myDataBase) {
+    passport.use(
+      new LocalStrategy((username, password, done) => {
+        myDataBase.findOne({ username: username }, (err, user) => {
+          console.log(`User ${username} attempted to log in.`);
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            return done(null, false);
+          }
+          if (!bcrypt.compareSync(password, user.password)) {
+            return done(null, false);
+          }
+          return done(null, user);
+        });
+      })
+    );
 
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`);
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false);
-      }
-      if (!bcrypt.compareSync(password, user.password)) {
-        return done(null, false);
-      }
-      return done(null, user);
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
     });
-  })
-);
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, doc);
-  });
-});
+    passport.deserializeUser((id, done) => {
+      myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+        done(null, doc);
+      });
+    });
+};
