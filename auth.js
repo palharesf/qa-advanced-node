1d91a24e3a1b44b7a530a265214f9f6fe456ecb2
Fernando <palhares.f@gmail.com>
2024-12-23 12:02:50 -0300

    Implemented GitHub OAuth Strategy

--- auth.js
+++ auth.js
@@ -1,34 +1,51 @@
+require("dotenv").config();
+
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const bcrypt = require("bcrypt");
 const ObjectID = require("mongodb").ObjectID;
+const GitHubStrategy = require("passport-github").Strategy;
 
 module.exports = function (app, myDataBase) {
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+    );
+    
     passport.use(
-      new LocalStrategy((username, password, done) => {
-        myDataBase.findOne({ username: username }, (err, user) => {
-          console.log(`User ${username} attempted to log in.`);
-          if (err) {
-            return done(err);
-          }
-          if (!user) {
-            return done(null, false);
-          }
-          if (!bcrypt.compareSync(password, user.password)) {
-            return done(null, false);
-          }
-          return done(null, user);
-        });
-      })
+      new GitHubStrategy(
+        {
+          clientID: process.env.GITHUB_CLIENT_ID,
+          clientSecret: process.env.GITHUB_CLIENT_SECRET,
+          callbackURL: "http://localhost:3000/auth/github/callback",
+        },
+        function (accessToken, refreshToken, profile, cb) {
+          console.log(profile);
+          //Database logic here with callback containing your user object
+        }
+      )
     );
 
-    passport.serializeUser((user, done) => {
-      done(null, user._id);
-    });
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
 
-    passport.deserializeUser((id, done) => {
-      myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-        done(null, doc);
-      });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
     });
+  });
 };
