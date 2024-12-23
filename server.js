e2fd2ddac860fe5dd50a6a6a5022ec9adb2996f7
Fernando <palhares.f@gmail.com>
2024-12-23 10:51:04 -0300

    Implemented user creation logic

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ myDB(async client => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please log in',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true,
     });
   });
 
@@ -57,6 +58,38 @@ myDB(async client => {
       }
     });
   });
+
+  app.route("/register").post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: req.body.password,
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
+    }
+  );
 
   app.use((req, res, next) => {
     res.status(404)
